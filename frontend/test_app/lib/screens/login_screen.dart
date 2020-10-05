9df28c919bba453acc54fe991a18e0a651129c3c
Rodrigo Heiremans <rheiremans@uc.cl>
2020-10-05 10:39:51 -0300

    :sparkles: pantalla log in lista

---
+++ frontend/test_app/lib/screens/login_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _nickname, _password;
+
+  _submit() {
+    // Acá estamos revisando el input del usuario
+    if (_formKey.currentState.validate()){
+      _formKey.currentState.save();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Guatsap',
+              style: TextStyle(fontSize: 50.0),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 30.0,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Nickname'),
+                      validator: (input) => !input.contains('0')
+                          ? 'Please enter a valid nickname'
+                          : null,
+                      onSaved: (input) => _nickname = input,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 30.0,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Password'),
+                      validator: (input) => input.length < 6
+                          ? 'The password must have 6 characters'
+                          : null,
+                      onSaved: (input) => _password = input,
+                      obscureText: true,
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Container(
+                    width: 250.0,
+                    child: FlatButton(
+                      onPressed: _submit,
+                      color: Colors.redAccent,
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
