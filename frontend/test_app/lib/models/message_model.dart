cb38b354547e3a1853e687e733ce56b24c37536f
Rodrigo Heiremans <rheiremans@uc.cl>
2020-10-05 01:08:24 -0300

    :speech_balloon: chat sin text field

--- frontend/test_app/lib/models/message_model.dart
+++ frontend/test_app/lib/models/message_model.dart
@@ -85,31 +85,31 @@ List<Message> chats = [
 List<Message> messages = [
   Message(
     sender: perecic,
-    time: '5:30 PM',
-    text: 'Hola, que tal?',
+    time: '7:00 PM',
+    text: 'Me eché el ramo',
     unread: true,
   ),
   Message(
     sender: gerardo,
-    time: '5:30 PM',
-    text: 'Hola, que tal?',
+    time: '5:52 PM',
+    text: 'La API también grande cabros',
     unread: true,
   ),
   Message(
     sender: lucas,
-    time: '5:30 PM',
-    text: 'Hola, que tal?',
+    time: '5:50 PM',
+    text: 'Quedo pulenta la interfaz',
     unread: true,
   ),
   Message(
     sender: igo,
-    time: '5:30 PM',
-    text: 'Hola, que tal?',
+    time: '5:40 PM',
+    text: 'Que es esto',
     unread: false,
   ),
   Message(
     sender: ian,
-    time: '5:30 PM',
+    time: '5:36 PM',
     text: 'Hola, que tal?',
     unread: true,
   ),
@@ -118,5 +118,11 @@ List<Message> messages = [
     time: '5:30 PM',
     text: 'Que pasa loco',
     unread: true,
+  ),
+  Message(
+    sender: currentUser,
+    time: '5:20 PM',
+    text: "Buena grupo 3, se sacaron un 7",
+    unread: false,
   ),
 ];
--- frontend/test_app/lib/screens/chat_screen.dart
+++ frontend/test_app/lib/screens/chat_screen.dart
@@ -1,18 +1,125 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:test_app/models/message_model.dart';
 import 'package:test_app/models/user_model.dart';
 
 class ChatScreen extends StatefulWidget {
-
   final User user; // Aca le vamos a pasar el chatroom id
 
   ChatScreen({this.user});
+
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  _buildMessage(Message message, bool isMe) {
+    return Container(
+      margin: isMe
+          ? EdgeInsets.only(
+              top: 8.0,
+              bottom: 8.0,
+              left: 80.0,
+            )
+          : EdgeInsets.only(
+              top: 8.0,
+              bottom: 8.0,
+              right: 80.0,
+            ),
+      padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+      decoration: BoxDecoration(
+        color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
+        borderRadius: isMe
+            ? BorderRadius.only(
+                topLeft: Radius.circular(15.0),
+                topRight: Radius.circular(15.0),
+              )
+            : BorderRadius.only(
+                bottomRight: Radius.circular(15.0),
+                topRight: Radius.circular(15.0),
+              ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            message.time,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            message.text,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        title: Container(
+          alignment: Alignment.center,
+          child: Text(
+            widget.user.name,
+            style: TextStyle(
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+                child: ListView.builder(
+                  reverse: true,
+                  padding: EdgeInsets.only(top: 15.0),
+                  itemCount: messages.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final Message message = messages[index];
+                    final bool isMe = message.sender.id == currentUser.id;
+                    return _buildMessage(message, isMe);
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
