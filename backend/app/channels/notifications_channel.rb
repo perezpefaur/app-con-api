a25fa34a8f061f6c59157287378d71088271af3b
Ian Basly <igbasly@uc.cl>
2020-10-13 05:49:18 -0300

    :bug: Notifications channels minor error fixed

--- backend/app/channels/notifications_channel.rb
+++ backend/app/channels/notifications_channel.rb
@@ -2,11 +2,11 @@ class NotificationsChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     token = params['authorization_token']
-    user = User.find_by({authorization_token: token})
+    user = User.find_by({authentication_token: token})
     if user && user.id.to_s == params["user"].to_s
       # Open stream for a parameterized channel
     
-      stream_from "notification_user_#{user}"
+      stream_from "notification_user_#{user.id}"
     end
   end
 
